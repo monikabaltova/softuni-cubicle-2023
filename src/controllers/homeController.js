2e71f320cb08baded4f7b4d0f48f45cb79f70647
monikabaltova <monika.baltova31@gmail.com>
2023-01-23 22:13:34 +0000

    add surch

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,29 @@
 const db = require('../db.json');
 
 exports.getHomePage = (req, res) => {
-    res.render('index', { cubes: db.cubes });
+    const { search, from: difficultyFrom, to: difficaltyTo } = req.query;
+
+    let cubes = db.cubes;
+
+    if (search) {
+        cubes = cubes.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
+    }
+
+    if (difficultyFrom) {
+        cubes = cubes.filter(cube => cube.difficultyLevel >= difficultyFrom);
+    }
+
+    if (difficaltyTo) {
+        cubes = cubes.filter(cube => cube.difficultyLevel <= difficaltyTo);
+    }
+
+    res.render('index', { cubes, search , difficultyFrom, difficaltyTo });
 };
 
 exports.getAboutPage = (req, res) => {
     res.render('about')
+};
+
+exports.getErrorPage = (req, res) => {
+    res.render('404');
 };
