ebf8f91b29d58a94a9acccff7cbc2f8ab75a7ee9
monikabaltova <monika.baltova31@gmail.com>
2023-01-23 21:44:11 +0000

    add details page

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ router.get('/about', homeController.getAboutPage);
 //    res.render('create');
 //});
 router.get('/create',cubeController.getCreateCube);
-router.post('/create', cubeController.postCreateCube)
+router.post('/create', cubeController.postCreateCube);
+router.get('/details/:cubeId', cubeController.getDetails);
 
 module.exports = router;
