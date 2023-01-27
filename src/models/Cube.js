fe116c4e44e59dd1a6ac5c1e0645682551fa5067
monikabaltova <monika.baltova31@gmail.com>
2023-01-27 17:21:28 +0000

    save cube db

---
+++ src/models/Cube.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+
+const cubeSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: 50,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        //add http//htttps validation
+    },
+    difficultyLevel: {
+        type: Number,
+        required: true,
+        max: 6,
+        min: 1,
+    },
+    accessories: {
+        type: String,
+        required: true,
+    }
+});
+
+const Cube = model('Cube', cubeSchema);
+
+module.exports = Cube;
