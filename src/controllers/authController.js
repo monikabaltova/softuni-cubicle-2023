ac2080806ac42d2faaf1d83d86b8b4a30945d349
monikabaltova <monika.baltova31@gmail.com>
2023-02-03 15:43:32 +0000

    register users

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,31 @@
 const router = require('express').Router();
 
+const authService = require('../services/authService');
+
 router.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
 router.get('/register', (req, res) => {
     res.render('auth/register');
+});
+
+router.post('/register', async (req, res) => {
+    const { username, password, repeatPassword } = req.body;
+
+    if(password !== repeatPassword) {
+        return res.redirect('/404');
+    }
+
+    const existingUser = await authService.getUserByUsername(username);
+
+    if (existingUser) {
+        return res.redirect('/404');
+    }
+
+    const user = await authService.register(username, password);
+
+    res.redirect('/login');
 });
 
 module.exports = router;
---
+++ src/services/authService.js
@@ -0,0 +1,8 @@
+const User = require('../models/User');
+
+exports.getUserByUsername = (username) => {
+    return User.findOne({ username });
+}
+exports.register = async (username, password) => {
+    await User.create({username, password});
+};
