8bdc40e78dd02805febfd9e2416a6ab1e00dab66
monikabaltova <monika.baltova31@gmail.com>
2023-02-03 21:49:23 +0000

    show edit cube page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,5 +1,7 @@
 const Cube = require('../models/Cube');
 const Accessory = require('../models/Accessory');
+const cubeService = require('../services/cubeService');
+const cubeUtils = require('../utils/cubeUtils')
 
 exports.getCreateCube = (req, res) => {   
     console.log('req.user');
@@ -43,4 +45,18 @@ exports.postAttachAccessory = async (req, res) => {
     await cube.save();
 
     res.redirect(`/cubes/${cube._id}/details`);
+};
+
+exports.getEditCube = async (req, res) => {
+    const cube = await cubeService.getOne(req.params.cubeId).lean();
+    const difficultyLevels = cubeUtils.generateDifficultyLevels(cube.difficultyLevel);
+    
+    res.render('cube/edit', { cube, difficultyLevels });
+};
+
+
+exports.getDeleteCube = async (req, res) => {
+    const cube = await cubeService.getOne(req.params.cubeId).lean();
+    const difficultyLevels = cubeUtils.generateDifficultyLevels(cube.difficultyLevel);
+    res.render('cube/delete', { cube, difficultyLevels });
 };
